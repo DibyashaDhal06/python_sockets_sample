8f1a9461baa1d980a1417d60cb3be342aa84b429
Dibyasha Dhal <79873045+DibyashaDhal06@users.noreply.github.com>
2021-07-22 01:14:06 +0530

    Update client.py

--- client.py
+++ client.py
@@ -3,10 +3,10 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("localhost", 8080)) # Notice how we don't need to bother with FQDN here
-
-sent = sock.send("hello".encode()) # We use endline as a common terminator
-print(sent)
-resp = sock.recv(2048)
-print("Recieved ", resp)
-
-# sock.close()
+print("Connected")
+name = input('Enter your name: ')
+sent = sock.send(name.encode()) # We use endline as a common terminator
+print("Sent to server:", sent)
+resp = sock.recv(2048).decode('ascii')
+print(resp, "has connected ")
+sock.close()
